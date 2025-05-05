5a23cb3574e50e182cedfa6778c6fbea9bf06fd2
Connor Tamme <lrk312@usask.ca>
2025-05-05 15:57:47 -0600

    Added fanet-power.cc

--- ns-3.43/makeAllCharts.sh
+++ ns-3.43/makeAllCharts.sh
@@ -3,15 +3,15 @@
 # nodes sinks power name_of_experiment time
 
 
-#./makeChart.sh 30 1 1000 "Sanity Check" 400
+./makeChart.sh 30 1 1000 "Sanity Check" 400
 #./makeChart.sh 80 10 5 "High Nodes, High Sinks, Regular Power" 300
 #./makeChart.sh 80 2 5 "High Nodes, Low Sinks, Regular Power" 300
-./makeChart.sh 80 10 10 "High Nodes, High Sinks, High Power" 300
-./makeChart.sh 80 2 10 "High Nodes, Low Sinks, High Power" 300
-./makeChart.sh 30 10 5 "Low Nodes, High Sinks, Regular Power" 300
-./makeChart.sh 30 2 5 "Low Nodes, Low Sinks, Regular Power" 300
-./makeChart.sh 30 10 10 "Low Nodes, High Sinks, High Power" 300
-./makeChart.sh 30 2 10 "Low Nodes, Low Sinks, High Power" 300
+#./makeChart.sh 80 10 10 "High Nodes, High Sinks, High Power" 300
+#./makeChart.sh 80 2 10 "High Nodes, Low Sinks, High Power" 300
+#./makeChart.sh 30 10 5 "Low Nodes, High Sinks, Regular Power" 300
+#./makeChart.sh 30 2 5 "Low Nodes, Low Sinks, Regular Power" 300
+#./makeChart.sh 30 10 10 "Low Nodes, High Sinks, High Power" 300
+#./makeChart.sh 30 2 10 "Low Nodes, Low Sinks, High Power" 300
 
 
 
--- ns-3.43/makeChart.sh
+++ ns-3.43/makeChart.sh
@@ -6,9 +6,9 @@ echo $4
 for alg in OLSR AODV DSR DSDV;
 do
     date > ./time/"$4$alg".txt
-    ./ns3 run "manet-power --CSVfileName=$alg.csv --protocol=$alg --nWifis=$1 --nSinks=$2 --NewCSV=true --TotalTime=$5 --MaxY=500 --MaxX=300 --TPow=$3 --CSVSpecfileName=$alg.Spec.csv"
+    ./ns3 run "manet-power --CSVfileName=$alg-$4.csv --protocol=$alg --nWifis=$1 --nSinks=$2 --NewCSV=true --TotalTime=$5 --MaxY=500 --MaxX=300 --TPow=$3 --CSVSpecfileName=$alg.Spec.csv"
     date >> ./time/"$4$alg".txt
 done
-python ./makePlot.py "$4"
-python ./makeRate.py "$4"
-python ./makeNoSpecialPlot.py "$4"
+#python ./makePlot.py "$4"
+#python ./makeRate.py "$4"
+#python ./makeNoSpecialPlot.py "$4"
