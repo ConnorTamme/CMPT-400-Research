98d81fc8d13b15cd7afaa90d07b27c0c715b5ec0
Connor Tamme <lrk312@usask.ca>
2025-05-08 16:17:26 -0600

    Updated makeAllCharts.sh

--- makeAllCharts.sh
+++ makeAllCharts.sh
@@ -3,15 +3,15 @@
 # nodes sinks power name_of_experiment time
 
 
-./makeChart.sh 30 1 1000 "Sanity Check" 400
-#./makeChart.sh 80 10 5 "High Nodes, High Sinks, Regular Power" 300
-#./makeChart.sh 80 2 5 "High Nodes, Low Sinks, Regular Power" 300
-#./makeChart.sh 80 10 10 "High Nodes, High Sinks, High Power" 300
-#./makeChart.sh 80 2 10 "High Nodes, Low Sinks, High Power" 300
-#./makeChart.sh 30 10 5 "Low Nodes, High Sinks, Regular Power" 300
-#./makeChart.sh 30 2 5 "Low Nodes, Low Sinks, Regular Power" 300
-#./makeChart.sh 30 10 10 "Low Nodes, High Sinks, High Power" 300
-#./makeChart.sh 30 2 10 "Low Nodes, Low Sinks, High Power" 300
+#./makeChart.sh 30 1 1000 "Sanity_Check" 200
+./makeChart.sh 80 10 5 "High_Nodes,_High_Sinks,_Regular_Power" 300
+./makeChart.sh 80 2 5 "High_Nodes,_Low_Sinks,_Regular_Power" 300
+./makeChart.sh 80 10 10 "High_Nodes,_High_Sinks,_High_Power" 300
+./makeChart.sh 80 2 10 "High_Nodes,_Low_Sinks,_High_Power" 300
+./makeChart.sh 30 10 5 "Low_Nodes,_High_Sinks,_Regular_Power" 300
+./makeChart.sh 30 2 5 "Low_Nodes,_Low_Sinks,_Regular_Power" 300
+./makeChart.sh 30 10 10 "Low_Nodes,_High_Sinks,_High_Power" 300
+./makeChart.sh 30 2 10 "Low_Nodes,_Low_Sinks,_High_Power" 300
 
 
 
